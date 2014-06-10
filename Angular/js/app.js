3106d15e4bbc23b8ddb2a45d8c0e19a894732970
sergio <sergio@sergio-ideapath>
2014-06-10 08:55:34 -0300

    agrego usuario con materias

--- Angular/js/app.js
+++ Angular/js/app.js
@@ -5,14 +5,22 @@
 var systemApp = angular.module('systemApp', ['systemFilters']);
 
 systemApp.controller('SystemCtrl', function ($scope, $http) {
-    $http.get('data/courses.json').success(function(data) {
-        $scope.courses = data;
+    $http.get('data/user_1.json').success(function(data) {
+        $scope.user = data;
     });
     $http.get('data/subjects.json').success(function(data) {
         $scope.subjects = data;
     });
+    $http.get('data/courses.json').success(function(data) {
+        $scope.courses = data;
+    });
     
-    $scope.completed = function() {
-        return true;
+    $scope.completed = function(assignature) {
+        if ($scope.user.subjects !== undefined
+            && $scope.user.subjects.hasOwnProperty(assignature) 
+            && $scope.user.subjects[assignature].grade >= 4) {
+            return 0;
+        }
+        return 2;
     };
 });
