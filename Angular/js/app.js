d81d8d089526c84c589a6c7d6b29b7609cf30a5f
sergio <sergio@sergio-ideapath>
2014-06-13 01:18:33 -0300

    Agrego modal y logica para manejarla

--- Angular/js/app.js
+++ Angular/js/app.js
@@ -14,6 +14,16 @@ systemApp.controller('SystemCtrl', function ($scope, $http) {
     $http.get('data/courses.json').success(function(data) {
         $scope.courses = data;
     });
+    $http.get('data/next-courses.json').success(function(data) {
+        $scope.nextCourses = data;
+    });
+    
+    $scope.activeModal = false;
+    $scope.selectedCourse = {
+        'code': null,
+        'data': null,
+        'next': null
+    };
     
     $scope.completed = function(assignature) {
         if ($scope.user.subjects !== undefined
@@ -22,5 +32,20 @@ systemApp.controller('SystemCtrl', function ($scope, $http) {
             return 0;
         }
         return 2;
+    };
+    
+    $scope.toggleModal = function(assignature) {
+        $scope.activeModal = !$scope.activeModal;
+        if ($scope.courses !== undefined
+                && $scope.courses.hasOwnProperty(assignature)) {
+            $scope.selectedCourse.code = assignature;
+            $scope.selectedCourse.data = $scope.courses[assignature];
+            if ($scope.nextCourses !== undefined
+                    && $scope.nextCourses.hasOwnProperty(assignature)) {
+                $scope.selectedCourse.next = $scope.nextCourses[assignature];
+            }
+        } else {
+            $scope.selectedCourse.code = 'none';
+        }
     };
 });
